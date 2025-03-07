02d530a2890a7768b42c6cbda15592c69ba4c28e
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2025-03-06 21:28:56 -0600

    update

--- src/test/java/MacchiatoTest.java
+++ src/test/java/MacchiatoTest.java
@@ -8,35 +8,14 @@ import java.util.List;
 public class MacchiatoTest {
     public static void main(String[] args) {
 
-        // SELECT * FROM courses JOIN teachers ON (teachers.teacher_id = courses.teacher_id) WHERE teachers.first_name = "John" ;
-
         Macchiato.DATABASE = "jdbc:sqlite:test.db";
 
         TeacherRepository teacherRepository = new TeacherRepository();
         CourseRepository courseRepository = new CourseRepository();
 
-//        List<Teacher> teachers = teacherRepository.getAll();
-//        List<Course> courses = courseRepository.getAll();
-//
-//        List<Course> johnCourses = teacherRepository.findById(Integer.toString(101)).courses;
-//
-//        for (Course c : johnCourses) {
-//            System.out.println(String.format("(%s, %s)", c.courseId, c.courseName));
-//        }
 
         Teacher teacher = courseRepository.findById(Integer.toString(201)).teacher;
         System.out.println(teacher.firstName);
-
-//        System.out.println("Teachers");
-//        teachers.forEach((teacher) -> {
-//            System.out.println(String.format("(%s, %s, %s)", teacher.teacherId, teacher.firstName, teacher.lastName));
-//        });
-//
-//        System.out.println("Courses");
-//        courses.forEach((course) -> {
-//            System.out.println(String.format("(%s, %s, %s)", course.courseId, course.courseName, course.teacherId));
-//        });
-
 
 
     }
