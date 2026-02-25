0e008d681b230a15f8b3bb2be4ea70821c454f76
Gabriel Gavrilov <gavrilog@fcc.ca>
2026-02-25 15:44:03 -0600

    update

--- src/test/java/MacchiatoTest.java
+++ src/test/java/MacchiatoTest.java
@@ -1,13 +1,46 @@
-import org.gabrielgavrilov.macchiato.DataSource;
-import org.gabrielgavrilov.macchiato.Macchiato;
 
-import java.sql.*;
-import java.util.ArrayList;
+import org.gabrielgavrilov.macchiato.Macchiato;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class MacchiatoTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
+//        Macchiato.DATABASE = "jdbc:sqlite:test.db";
+        Macchiato.DATABASE = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
+
+        Connection conn = DriverManager.getConnection(Macchiato.DATABASE);
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate("""
+            CREATE TABLE IF NOT EXISTS teachers (
+                teacher_id INT PRIMARY KEY,
+                first_name VARCHAR(255),
+                last_name VARCHAR(255)
+            );
+        """);
+
+        stmt.close();
+        conn.close();
+
+
+
+        TeacherRepository teacherRepository = new TeacherRepository();
+        CourseRepository courseRepository = new CourseRepository();
+
+        teacherRepository.save(Teacher.newInstance(1, "John", "Doe"));
+
+        List<Teacher> teachers = teacherRepository.getAll();
+
+        for (Teacher teacher : teachers) {
+            System.out.println(teacher.firstName);
+        }
 
     }
+
+
 }
